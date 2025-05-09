b58c28410a83ca1300d0203beb88d4ca0ca7eaf3
Mauyolo <mgrondon16@gmail.com>
2025-05-09 18:20:59 -0500

    Add files via upload
    Agregar aplicador de descuentos con pruebas

--- src/main/java/tudelft/discount/DiscountApplier.java
+++ src/main/java/tudelft/discount/DiscountApplier.java
@@ -4,20 +4,21 @@ public class DiscountApplier {
 
     private ProductDao dao;
 
-    public DiscountApplier (ProductDao dao) {
+    public DiscountApplier(ProductDao dao) {
         this.dao = dao;
     }
 
     public void setNewPrices() {
-
-        for(Product product : dao.all()) {
-            if(product.getCategory().equals("BUSINESS")) {
-                product.setPrice(product.getPrice() * 0.9);
-            }
-            if(product.getCategory().equals("HOME")) {
+        for (Product product : dao.all()) {
+            if (product.getCategory().equals("BUSINESS")) {
+                // Aumento de 10% para "BUSINESS"
                 product.setPrice(product.getPrice() * 1.1);
             }
+            if (product.getCategory().equals("HOME")) {
+                // Descuento de 10% para "HOME"
+                product.setPrice(product.getPrice() * 0.9);
+            }
         }
-
     }
 }
+
